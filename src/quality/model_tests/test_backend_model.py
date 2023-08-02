8fda703726c2d443635c1ab01338bd9210e3543e
Alexander Hering <dev.hering@gmail.com>
2023-08-03 01:24:03 +0200

    starting backend model tests

---
+++ src/quality/model_tests/test_backend_model.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*          Basic Language Model Backend            *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import unittest
+from src.model.backend_control.dataclasses import create_or_load_database
+from src.model.backend_control.llm_pool import LLMPool
+
+
+class DataclassesTest(unittest.TestCase):
+    """
+    Test case class for testing URL configuration.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Class method for setting up test case.
+        """
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Class method for setting tearing down test case.
+        """
+        pass
+
+    @classmethod
+    def setup_class(cls):
+        """
+        Alternative class method for setting up test case.
+        """
+        cls.setUpClass()
+
+    @classmethod
+    def teardown_class(cls):
+        """
+        Alternative class for setting tearing down test case.
+        """
+        cls.tearDownClass()
+
+
+class LLMPoolTest(unittest.TestCase):
+    """
+    Test case class for testing URL configuration.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Class method for setting up test case.
+        """
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Class method for setting tearing down test case.
+        """
+        pass
+
+    @classmethod
+    def setup_class(cls):
+        """
+        Alternative class method for setting up test case.
+        """
+        cls.setUpClass()
+
+    @classmethod
+    def teardown_class(cls):
+        """
+        Alternative class for setting tearing down test case.
+        """
+        cls.tearDownClass()
+
+
+if __name__ == '__main__':
+    unittest.main()
