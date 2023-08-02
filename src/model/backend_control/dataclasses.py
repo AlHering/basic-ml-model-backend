8f3c7e96f61a0b658f0dbbdce0dcb3794694df00
Alexander Hering <dev.hering@gmail.com>
2023-08-02 16:35:51 +0200

    added uuid creation as server default

--- src/model/backend_control/dataclasses.py
+++ src/model/backend_control/dataclasses.py
@@ -5,6 +5,7 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+from uuid import uuid4
 from sqlalchemy import Column, String, JSON, ForeignKey, BLOB, Integer, DateTime, func, text
 from sqlalchemy.ext.automap import automap_base, classname_for_table
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,10 +13,17 @@ from sqlalchemy.orm import relationship
 from src.utility.bronze import sqlalchemy_utility
 
 
+# Taken from https://stackoverflow.com/questions/17277735/using-uuids-in-sqlite and adjusted
+UUID_CREATION = text("""cast( lower(hex( randomblob(4)) || '-' || hex( randomblob(2))
+         || '-' || '4' || substr( hex( randomblob(2)), 2) || '-'
+         || substr('AB89', 1 + (abs(random()) % 4) , 1)  ||
+         substr(hex(randomblob(2)), 2) || '-' || hex(randomblob(6))) as varchar)""")
+
+
 MODEL = {
     "__tablename__": "model",
     "__table_args__": {"comment": "Model Table."},
-    "uuid": Column(String, primary_key=True, unique=True, nullable=False,
+    "uuid": Column(String, primary_key=True, unique=True, nullable=False, server_default=UUID_CREATION,
                    comment="UUID of the model."),
     "path": Column(String, nullable=False,
                    comment="Path of the model."),
@@ -24,23 +32,23 @@ MODEL = {
     "model_loader": Column(String, nullable=False,
                            comment="Loader for the model."),
     "created": Column(DateTime, server_default=func.now(),
-                             comment="Timestamp of creation."),
+                      comment="Timestamp of creation."),
     "updated": Column(DateTime, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
-                             comment="Timestamp of last update.")
+                      comment="Timestamp of last update.")
 }
 
 
 INSTANCE = {
     "__tablename__": "instance",
     "__table_args__": {"comment": "Instance Table."},
-    "uuid": Column(String, primary_key=True, unique=True, nullable=False,
-                   comment="UUID of the model instance."),
+    "uuid": Column(String, primary_key=True, unique=True, nullable=False, server_default=UUID_CREATION,
+                   comment="UUID of the model."),
     "model_uuid": Column(String, ForeignKey(f"model.uuid"),
-                                   comment="Registered model to use."),
+                         comment="Registered model to use."),
     "created": Column(DateTime, server_default=func.now(),
-                             comment="Timestamp of creation."),
+                      comment="Timestamp of creation."),
     "updated": Column(DateTime, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
-                             comment="Timestamp of last update.")
+                      comment="Timestamp of last update.")
 }
 
 
@@ -48,14 +56,14 @@ LOG = {
     "__tablename__": "log",
     "__table_args__": {"comment": "Logging Table."},
     "id": Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False,
-                   comment="ID of the logging entry."),
+                 comment="ID of the logging entry."),
     "request": Column(JSON, nullable=False,
-                              comment="Request, sent to the backend."),
+                      comment="Request, sent to the backend."),
     "response": Column(JSON, comment="Response, given by the backend."),
     "started": Column(DateTime, server_default=func.now(),
-                             comment="Timestamp of request recieval."),
+                      comment="Timestamp of request recieval."),
     "finished": Column(DateTime, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
-                             comment="Timestamp of reponse transmission.")
+                       comment="Timestamp of reponse transmission.")
 
 }
 
