1b30cbff84c34972931bed4c5047e3604f92696f
Alexander Hering <dev.hering@gmail.com>
2023-08-02 19:36:27 +0200

    implemented basic wrappers for live testing

--- src/utility/gold/language_model_utility.py
+++ src/utility/gold/language_model_utility.py
@@ -5,19 +5,38 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
-from typing import Any, Optional
+import os
+from typing import Any, Optional, List
 from abc import ABC, abstractmethod
-from pydantic import BaseModel
+from transformers import AutoTokenizer, AutoModel
+import torch.nn.functional as F
+from torch import Tensor
 from langchain.llms import LlamaCpp
+from src.configuration import configuration as cfg
 
 """
 Loader classes
 """
 
 
-class LlamaCppWrapper(object):
+class LanguageModel(ABC):
     """
-    Wrapper class for LlamaCpp.
+    Abstract language model class.
+    """
+
+    @abstractmethod
+    def generate(prompt: str) -> Any:
+        """
+        Main handler method for wrapping language model capabilities.
+        :param prompt: User prompt.
+        :return: Response.
+        """
+        pass
+
+
+class LlamaCppLM(LanguageModel):
+    """
+    General LM class for LlamaCpp.
     """
 
     def __init__(self, representation: dict) -> None:
@@ -26,16 +45,89 @@ class LlamaCppWrapper(object):
         :param representation: Language model representation.
         """
         self.llm = LlamaCpp(
+            model_path=os.path.join(
+                cfg.PATHS.TEXTGENERATION_MODEL_PATH, representation["path"]),
+            n_ctx=representation["context"],
+            verbose=representation["verbose"]
+        )
 
-        ).generate()
-
-    def generate(self, query: str) -> Optional[Any]:
+    def generate(self, prompt: str) -> Optional[Any]:
         """
         Generation method.
-        :param query: User query.
+        :param prompt: User prompt.
         :return: Response, if generation method is available else None.
         """
-        return self.llm.generate([query])
+        return self.llm.generate([prompt])
+
+
+class LocalHFLM(LanguageModel):
+    """
+    General LM class for local Huggingface models.
+    """
+
+    def __init__(self, representation: dict) -> None:
+        """
+        Initiation method.
+        :param representation: Language model representation.
+        """
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            pretrained_model_name_or_path=representation["path"],
+            local_files_only=True)
+        self.model = AutoModel.from_pretrained(
+            pretrained_model_name_or_path=representation["path"],
+            local_files_only=True)
+
+    def generate(self, prompt: str) -> Optional[Any]:
+        """
+        Generation method.
+        :param prompt: User prompt.
+        :return: Response, if generation method is available else None.
+        """
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        return self.model(**inputs)
+
+
+class LocalHFEmbeddingModel(LanguageModel):
+    """
+    General LM class for local Huggingface models for embedding.
+    """
+
+    def __init__(self, representation: dict) -> None:
+        """
+        Initiation method.
+        :param representation: Language model representation.
+        """
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            pretrained_model_name_or_path=representation["path"],
+            local_files_only=True)
+        self.model = AutoModel.from_pretrained(
+            pretrained_model_name_or_path=representation["path"],
+            local_files_only=True)
+
+    def generate(self, prompt: str) -> List[float]:
+        """
+        Method for embedding prompt.
+        :param prompt: Prompt.
+        :return: Prompt embedding.
+        """
+        inputs = self.tokenizer(prompt, max_length=512,
+                                padding=True, truncation=True, return_tensors='pt')
+
+        outputs = self.model(**inputs)
+        embeddings = self.average_pool(outputs.last_hidden_state,
+                                       inputs['attention_mask'])
+
+        # normalize embeddings
+        embeddings = F.normalize(embeddings, p=2, dim=1)
+        return embeddings.tolist()
+
+    def average_pool(self, last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+        """
+        Average pooling function, taken from https://huggingface.co/intfloat/e5-large-v2.
+        """
+        last_hidden = last_hidden_states.masked_fill(
+            ~attention_mask[..., None].bool(), 0.0)
+        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
 
 """
