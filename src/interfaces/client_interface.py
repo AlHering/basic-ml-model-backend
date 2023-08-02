20766f240345234eebc65c527016d4cfe5882645
Alexander Hering <dev.hering@gmail.com>
2023-08-02 15:47:13 +0200

    adding client for easier usage in the future

---
+++ src/interfaces/client_interface.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*          Basic Language Model Backend            *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+# TODO: Implement client interface
