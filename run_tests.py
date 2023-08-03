866aeb07e7563853c61a88336081655080fff00b
Alexander Hering <dev.hering@gmail.com>
2023-08-03 22:23:44 +0200

    integrated tests into main test suite

--- run_tests.py
+++ run_tests.py
@@ -7,10 +7,13 @@
 """
 import unittest
 from src.quality.configuration_tests import test_configuration
+from src.quality.model_tests import test_backend_model
 
-
-suite = unittest.TestLoader().loadTestsFromModule(test_configuration)
+loader = unittest.TestLoader()
+suite = unittest.TestSuite()
+suite.addTests(loader.loadTestsFromModule(test_configuration))
+suite.addTests(loader.loadTestsFromModule(test_backend_model))
 
 
 if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.TextTestRunner(verbosity=3).run(suite)
