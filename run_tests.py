0f3e9329b63c2ba174ca0d198909833f7ed3e0f4
Alexander Hering <dev.hering@gmail.com>
2023-08-07 15:34:41 +0200

    marked as incomplete due to problems with llm test

--- run_tests.py
+++ run_tests.py
@@ -8,12 +8,17 @@
 import unittest
 from src.quality.configuration_tests import test_configuration
 from src.quality.model_tests import test_data_model
+from src.quality.model_tests import test_llm_pool
+from src.quality.control_tests import test_backend_controller
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 suite.addTests(loader.loadTestsFromModule(test_configuration))
 suite.addTests(loader.loadTestsFromModule(test_data_model))
+suite.addTests(loader.loadTestsFromModule(test_llm_pool))
+suite.addTests(loader.loadTestsFromModule(test_backend_controller))
 
 
 if __name__ == "__main__":
+    # TODO: Investigate problems with multithreaded llm handling in backend controller tests.
     unittest.TextTestRunner(verbosity=3).run(suite)
