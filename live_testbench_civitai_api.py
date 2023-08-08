8ba38aa6925ea5b7f9a0d5c7f87370909bd67551
Alexander Hering <dev.hering@gmail.com>
2023-08-08 22:29:53 +0200

    live testing

--- live_testbench_civitai_api.py
+++ live_testbench_civitai_api.py
@@ -5,4 +5,35 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+import os
 import requests
+import json
+from src.utility.bronze import json_utility
+from src.configuration import configuration as cfg
+from src.model.model_control.model_database import ModelDatabase, DEFAULT_DB_PATH
+from src.model.model_control.api_wrappers import CivitaiAPIWrapper
+
+
+RELIBERATE_MODEL_ID = 79754
+
+if __name__ == "__main__":
+    model_api_url_by_id = f"https://civitai.com/api/v1/model-versions"
+
+    db = ModelDatabase(database_uri=None, schema="civitai", verbose=True)
+    model_id = db.post_object("model",
+                              **{
+                                  "path": "myPath",
+                                  "architecture": "sd1.5",
+                                  "url": model_api_url_by_id
+                              })
+    wrapper = CivitaiAPIWrapper()
+
+    print(f"Check connection: {wrapper.check_connection()}")
+    print(
+        f"Check validation: {wrapper.validate_url_responsiblity(model_api_url_by_id)}")
+    print(
+        f"Fetch metadata ...")
+    model = db.get_object_by_id("model", model_id)
+    print(model)
+    json_utility.save(wrapper.collect_metadata(
+        "model", model), DEFAULT_DB_PATH + ".json")
