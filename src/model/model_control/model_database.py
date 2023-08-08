61ad356d1428d9e8504f2c1d21f91b9b0712385d
Alexander Hering <dev.hering@gmail.com>
2023-08-08 18:30:21 +0200

    fixed default database path

--- src/model/model_control/model_database.py
+++ src/model/model_control/model_database.py
@@ -13,7 +13,7 @@ from src.utility.gold.filter_mask import FilterMask
 from src.configuration import configuration as cfg
 
 
-DEFAULT_PATH = f"sqlite:///{cfg.PATHS.BACKEND_PATH}/model.db"
+DEFAULT_DB_PATH = f"{cfg.PATHS.BACKEND_PATH}/model.db"
 
 
 class ModelDatabase(object):
@@ -36,7 +36,7 @@ class ModelDatabase(object):
         self._logger.info("Automapping existing structures")
         self.base = automap_base()
         self.engine = sqlalchemy_utility.get_engine(
-            cfg.ENV.get("MODEL_DB", DEFAULT_PATH) if database_uri is None else database_uri)
+            cfg.ENV.get("MODEL_DB", f"sqlite:///{DEFAULT_DB_PATH}") if database_uri is None else database_uri)
         self.base.prepare(autoload_with=self.engine)
         self.session_factory = sqlalchemy_utility.get_session_factory(
             self.engine)
