6a8260ef05f4d455073d46037006b4ad09c7a808
Alexander Hering <dev.hering@gmail.com>
2023-08-09 22:54:01 +0200

    live testing

--- live_testbench_civitai_api.py
+++ live_testbench_civitai_api.py
@@ -9,31 +9,25 @@ import os
 import requests
 import json
 from src.utility.bronze import json_utility
+from src.utility.gold.filter_mask import FilterMask
 from src.configuration import configuration as cfg
 from src.model.model_control.model_database import ModelDatabase, DEFAULT_DB_PATH
+from src.model.model_control.model_handlers import DiffusionModelHandler
+
 from src.model.model_control.api_wrappers import CivitaiAPIWrapper
 
 
-RELIBERATE_MODEL_ID = 79754
-
 if __name__ == "__main__":
-    model_api_url_by_id = f"https://civitai.com/api/v1/models/{RELIBERATE_MODEL_ID}"
-
+    # if os.path.exists(DEFAULT_DB_PATH):
+    #    os.remove(DEFAULT_DB_PATH)
     db = ModelDatabase(database_uri=None, schema="civitai", verbose=True)
-    model_id = db.post_object("model",
-                              **{
-                                  "path": "myPath",
-                                  "architecture": "sd1.5",
-                                  "url": model_api_url_by_id
-                              })
     wrapper = CivitaiAPIWrapper()
-
-    print(f"Check connection: {wrapper.check_connection()}")
-    print(
-        f"Check validation: {wrapper.validate_url_responsiblity(model_api_url_by_id)}")
-    print(
-        f"Fetch metadata ...")
-    model = db.get_object_by_id("model", model_id)
-    print(model)
-    json_utility.save(wrapper.safely_fetch_api_data(
-        model_api_url_by_id), DEFAULT_DB_PATH + ".json")
+    handler = DiffusionModelHandler(
+        database=db,
+        model_folder=cfg.PATHS.TEST_PATH + "/diffusion_models",
+        cache_path=cfg.PATHS.TEST_PATH + "/diffusion_models_cache.json",
+        apis={
+            wrapper.get_source_name(): wrapper,
+        }
+    )
+    handler.scrape_available_models(wrapper.get_source_name())
